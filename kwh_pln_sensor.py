84241e391cba2f3ef70b5e50a62328444e5e5496
Jibril <jibrilhp@outlook.com>
2021-03-07 13:56:41 +0700

    ready~

--- kwh_pln_sensor.py
+++ kwh_pln_sensor.py
@@ -1,8 +1,8 @@
+from sqlalchemy import create_engine
 import cv2
 import numpy as np
 import pandas as pd
 import datetime
-from sqlalchemy import create_engine
 import time
 
 conn = create_engine("postgresql+psycopg2://root:12345678@localhost:5432/plnstats").connect()
@@ -19,16 +19,17 @@ while capture_status:
 
     result = frame.copy()
     size = result.size
+
     image = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
     lower = np.array([168, 25, 0])
     upper = np.array([179, 255, 255])
     mask = cv2.inRange(image, lower, upper)
-    result = cv2.bitwise_and(result, result, mask=mask)
 
     no_red = cv2.countNonZero(mask)
     frac_red = np.divide(float(no_red), size)
     percent_red = np.multiply((float(frac_red)), 100)
 
+    
     if (percent_red >= 10.0): #estimated from the red colour percentage impulse LED lamp
         data_capture = {
             'color_percentage': percent_red,
@@ -37,8 +38,6 @@ while capture_status:
 
         df = pd.DataFrame(columns=['color_percentage','created_on'],data=data_capture,index=[0])
         df.to_sql('home_pln_kwh_sensor', schema='public', con=conn, if_exists='append',index=False)
-
-
 
 cap.release()
 cv2.destroyAllWindows()
